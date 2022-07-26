f3f1f01d31ab80123c7d74d52b7984c7646473c0
Adeleke Oluwafemi <62451630+adeleke-code@users.noreply.github.com>
2022-07-27 00:32:58 +0100

    first commit

---
+++ mysite/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from django.http.response  import HttpResponse
+
+
+def home(request):
+    context = {
+        "page": "home"
+    }
+    return render(request, "home.html", context)
+
+def about(request):
+    data = {
+        "page": "about"
+    }
+    return render(request, "about.html", data)
+
+
+def services(request):
+    context = {
+        "page": "services"
+    }
+    return render(request, "services.html", context)
+
+
+def acheivements(request):
+    context = {
+        "page": "acheivements"
+    }
+    return render(request, "acheivements.html", context)
+
+
+def skills(request):
+    context = {
+        "page": "skills"
+    }
+    return render(request, "skills.html", context)
