53091645b7fea661578623a69fbec0fe2016e614
Adeleke Oluwafemi <62451630+adeleke-code@users.noreply.github.com>
2022-07-27 00:56:07 +0100

    fix

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
+
+app_name = 'mysite'
+
+
 urlpatterns = [
     path("",  views.home, name="home"),
     path("about/",  views.about, name="about_page"),
