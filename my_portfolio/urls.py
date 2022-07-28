0075c77452f4fabc7599caad4b59f82f99775e45
Adeleke Oluwafemi <62451630+adeleke-code@users.noreply.github.com>
2022-07-28 15:51:08 +0100

    fix

--- my_portfolio/urls.py
+++ my_portfolio/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('Home', include('mysite.urls'))
+    path('home/', include('mysite.urls'))
 ]
